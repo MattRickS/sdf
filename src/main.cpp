f5b53bd50363a2a68467ca351729fbccecf20ad8
MattRickS <mattalexshaw@gmail.com>
2020-07-11 15:14:02 +0100

    main example

--- src/main.cpp
+++ src/main.cpp
@@ -10,27 +10,37 @@
 
 static const float deg2rad = M_PI / 180;
 
+float rubix(const vec::vec3 &pos)
+{
+    static const vec::vec3 t(0.0f, 3.2f, 0.0f);
+    static const vec::mat4 m = vec::mat4::transformed(t);
+    static const vec::vec3 boxes(1.0f);
+    return sdf::box(
+        sdf::repeat(sdf::transform(pos, m), 2.2f, boxes),
+        boxes);
+}
+
 float distance(const vec::vec3 &pos)
 {
-    // static const vec::mat4 m = vec::mat4::rotated(vec::vec3(45.0f, 45.0f, 0.0f) * deg2rad);
-    static const vec::mat4 m = vec::mat4::identity();
-    return sdf::box(sdf::transform(pos, m), vec::vec3(2.0f, 2.0f, 2.0f));
-    // return sdf::box(
-    //     // Infinite repetition, abs() extends into negative, 3x3x3 spacing
-    //     sdf::repeat(abs(sdf::transform(pos, m)), vec::vec3(5.0f, 5.0f, 5.0f)),
-    //     // Box dimensions
-    //     vec::vec3(1.0f, 1.0f, 1.0f));
+    static const float sphereR{3.0f};
+    static const vec::vec3 sphereT(10.0f, sphereR, 0.0f);
+    static const vec::mat4 sphereM = vec::mat4::transformed(sphereT);
+    return std::min(
+        std::min(
+            sdf::plane(pos, vec::vec3::up(), 0.0f),
+            rubix(pos)),
+        sdf::sphere(sdf::transform(pos, sphereM), sphereR));
 }
 
 int main(int argc, char const *argv[])
 {
     vec::mat4 camTransform = vec::mat4::targetted(
-        vec::vec3(-3.0f, 5.0f, -20.0f),
-        vec::vec3(0.0f),
+        vec::vec3(-15.0f, 13.0f, -30.0f),
+        vec::vec3(0.0f, 3.0f, 0.0f),
         vec::vec3(0.0f, 1.0f, 0.0f));
     // 35mm sensor
     render::Camera camera(camTransform, 50.0f, 36.0f, 24.0f);
-    render::Buffer buffer(300, 100);
+    render::Buffer buffer(354, 200);
 
     render::Renderer renderer(distance);
     renderer.render(camera, buffer, 100.0f);
